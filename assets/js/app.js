f3bb3e19965de879415abee3773e6ae3f9ba6d97
coderjibon <contact.engjibon@gmail.com>
2021-08-31 19:08:42 +0600

    fuljag project

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+
+// A $( document ).ready() block.
+(function($){
+
+    $('.ourSliderArea').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 4,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
+
+
+
+
+})(jQuery);
